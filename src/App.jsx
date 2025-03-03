e3c71bebdabfd5743c841bf44fa73f7d16c70e43
OnlineProjectsDev <info@onlineprojects.com.au>
2025-03-03 16:23:11 +1100

    1.01

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import StepForm from "./StepForm";
 
 function App() {
   return (
-    <div className="flex justify-items-stretch min-h-screen bg-gray-100">
-      <div className="w-full bg-black h-screen p-6"></div>
+    <div className="">
       <StepForm />
     </div>
   );
